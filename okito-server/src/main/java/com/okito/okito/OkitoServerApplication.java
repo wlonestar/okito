6acd07e5f3be1aea6bfb46807f709257c97f1b82
wlonestar <wlonestar@163.com>
2023-01-24 22:53:46 +0800

    feat(server): create project

---
+++ okito-server/src/main/java/com/okito/okito/OkitoServerApplication.java
@@ -0,0 +1,13 @@
+package com.okito.okito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkitoServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OkitoServerApplication.class, args);
+  }
+
+}
---
+++ okito-server/src/test/java/com/okito/okito/OkitoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okito.okito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkitoServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
