f9854496679120650e7dc23c2fd9acdc84811cdb
wlonestar <wlonestar@163.com>
2023-02-25 20:09:06 +0800

    fix(server): move some method

--- okito-server/src/main/java/com/okito/okito/modules/posts/repository/TagFollowRepository.java
+++ okito-server/src/main/java/com/okito/okito/modules/posts/repository/TagFollowRepository.java
@@ -19,18 +19,6 @@ import java.util.List;
 @Repository
 public interface TagFollowRepository extends JpaRepository<TagFollow, TagFollowId> {
 
-  // TODO: move to user repository
-  @Query(value = "select new UserView(u.id, u.username, u.password, u.email, u.token, u.avatar, u.bio, u.homepage, " +
-      "u.intro, u.joinTime, u.followerNum, u.followedNum, u.postViewNum, u.postLikeNum) " +
-      "from UserView u where u.id in (select tf.id.userId from TagFollow tf where tf.id.tagId = ?1)")
-  List<UserView> findAllByTagId(Long tagId);
-
-  // TODO: move to user repository
-  @Query(value = "select new UserView(u.id, u.username, u.password, u.email, u.token, u.avatar, u.bio, u.homepage, " +
-      "u.intro, u.joinTime, u.followerNum, u.followedNum, u.postViewNum, u.postLikeNum) " +
-      "from UserView u where u.id in (select tf.id.userId from TagFollow tf where tf.id.tagId = ?1)")
-  Page<UserView> findAllByTagId(Long tagId, Pageable pageable);
-
   @Query(value = "select count(*) from tag_follow where user_id = ?1 and follow = true", nativeQuery = true)
   long countAllByUserId(Long userId);
 
--- okito-server/src/main/java/com/okito/okito/modules/users/repository/UserViewRepository.java
+++ okito-server/src/main/java/com/okito/okito/modules/users/repository/UserViewRepository.java
@@ -25,4 +25,9 @@ public interface UserViewRepository extends JpaRepository<UserView, Long> {
     "from UserView u where u.id in (select uf.id.followerId from UserFollow uf where uf.id.followedId = ?1 and uf.follow = true)")
   List<UserView> findAllFollowerByUserId(Long userId);
 
+  @Query(value = "select new UserView(u.id, u.username, u.password, u.email, u.token, u.avatar, u.bio, u.homepage, " +
+    "u.intro, u.joinTime, u.followerNum, u.followedNum, u.postViewNum, u.postLikeNum) " +
+    "from UserView u where u.id in (select tf.id.userId from TagFollow tf where tf.id.tagId = ?1)")
+  List<UserView> findAllByTagId(Long tagId);
+
 }
